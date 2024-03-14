53fd66b2c0c47bcc49124a8f748d4c6c6c9d9b57
root <root@C20240227199846.local>
2024-03-14 11:16:20 +0000

    [Feat] Create notepad migration struct

---
+++ notepad/database/migrations/2024_03_14_103558_create_notes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('note_id');
+            $table->string('title')->nullable(false);
+            $table->string('content')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
