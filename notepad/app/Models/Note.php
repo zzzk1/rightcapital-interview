2679266f4339f5ceae25a2cbff078cecf245960f
zzzk1 <madzhou1@gmail.com>
2024-03-14 15:53:56 +0000

    [Future][Backend] introduce a tag & note many to many method

--- notepad/app/Models/Note.php
+++ notepad/app/Models/Note.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * This class can be used to operate table note.
+ */
 class Note extends Model
 {
     protected $table = 'notes';
-    protected $primaryKey = 'note_id';
     use HasFactory;
+
+    /**
+     * This method use to find many tags for target note.
+     *
+     * @return BelongsToMany A lot of tag classes or only one, maybe none.
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'notes_tags');
+    }
 }
--- notepad/app/Models/Tag.php
+++ notepad/app/Models/Tag.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * This class can be used to operate table tag.
+ */
 class Tag extends Model
 {
     protected $table = 'tags';
-    protected $primaryKey = 'note_id';
     use HasFactory;
+
+    /**
+     * This method use to find many notes for target note.
+     *
+     * @return BelongsToMany A lot of note classes or only one, maybe none.
+     */
+    public function notes(): BelongsToMany
+    {
+        return $this->belongsToMany(Note::class, 'notes_tags');
+    }
 }
