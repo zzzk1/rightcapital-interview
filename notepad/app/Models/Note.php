fb40c9917b39bd9f9ea890e35723776bbe8f65b2
zzzk1 <madzhou1@gmail.com>
2024-03-15 13:53:02 +0000

    [Future][Backend] Fix Model Note & Tag & NoteTag support soft delete

--- notepad/app/Models/Note.php
+++ notepad/app/Models/Note.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
@@ -12,6 +13,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Note extends Model
 {
     protected $table = 'notes';
+    protected $dates = ['deleted_at'];
     use HasFactory;
     use SoftDeletes;
     
--- notepad/app/Models/NoteTag.php
+++ notepad/app/Models/NoteTag.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class NoteTag extends Model
 {
     protected $table = 'notes_tags';
+    protected $dates = ['deleted_at'];
     use HasFactory;
     use SoftDeletes;
 }
