5028ee75c7b7d11f1e239f63ca957ebfe1006e2b
zzzk1 <madzhou1@gmail.com>
2024-03-16 12:21:43 +0000

    [Future][Backend] Introduce a request data validator

---
+++ notepad/app/Http/Requests/CreateNotePadeRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateNotePadeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            // The title is required and must be a string. The maximum length is 100 and cannot be all spaces.
+            'title' => ['required', 'string', 'max:100', 'regex:/^\S/'],
+            // Content is required to be string.
+            'content' => 'required|string',
+            ];
+    }
+}
---
+++ notepad/app/Http/Requests/UpdateNotePadeRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateNotePadeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            // The title is required and must be a string. The maximum length is 100 and cannot be all spaces.
+            'title' => ['required', 'string', 'max:100', 'regex:/^\S/'], 
+            // Content is required to be string.
+            'content' => 'required|string',
+            // tagIds must be array.
+            'tagIds' => 'required|array', 
+        ];
+    }
+}
