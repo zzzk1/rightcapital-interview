1767a31b21880e8982ac141d5be1f655615bae1b
zzzk1 <madzhou1@gmail.com>
2024-03-16 05:59:10 +0000

    [Refactor] Improve NotePadController getDetail & update logical

--- notepad/app/Http/Controllers/NotePadController.php
+++ notepad/app/Http/Controllers/NotePadController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Note;
+use App\Models\Tag;
+use App\Models\NoteTag;
 
 class NotePadController extends Controller
 {
@@ -11,37 +13,58 @@ class NotePadController extends Controller
      * This method return a notepad, the notepad contains note title, note context and many tags.
      * 
      * @Param noteId Table note primary key.
-     * @Return A real notepad view.
+     * @Return detail data.
      */
     public function getDetail(string $noteId)
     {
         $note = Note::findOrFail($noteId);
-        $tags = $note->tags;
+        $tagList = $note->tags;
    
-        return view('notepad.detail', [
+        $data = [
             'note' => $note,
-            'tags' => $tags
-        ]);
+            'tags' => $tagList
+        ];
+
+        return response()->json($data);
     }
 
     /**
      * This method use to update a notepad by note id, the notepad contains note title, note context and many tags.
+     * We choice delete old bind relationship first, then bind new relationship.
      * 
+     * @Param request This request carries Note and list of Tags belonging to it.
      * @Param noteId Table note primary key.
-     * @Param request This request carries note data that needs to be updated.
      * @Return Whether the update operation was successful.
      */
     public function update(Request $request, $noteId)
     {
-        $note = Note::find($noteId);
+        // Fisrt update note's title and content.
+        $note = Note::findOrFail($noteId);
         $note->title = $request->title;
         $note->content = $request->content;
+        $note->save();
 
-        //TODO: update note tag relations
-    
+        // delete all old relationship.
+        NoteTag::where('note_id', $noteId)->delete();
 
-        //TODO: return updated view
+        // bind new relationship.
+        $tagIdList = $request->tagIds;
 
-        return $note->save();
+        // TODO: This operation may not perform well
+        foreach ($tagIdList as $tagId) {
+            NoteTag::create([
+                'note_id' => $noteId,
+                'tag_id' => $tagId
+            ]);
+        }
+        
+        $tagList = $note->tags;
+
+        $data = [
+            'note' => $note,
+            'tags' => $tagList
+        ];
+
+        return response()->json($data);
     }
 }
