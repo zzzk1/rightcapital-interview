ecccb4b39585144ee12dbd46bbd15870a2e68371
zzzk1 <madzhou1@gmail.com>
2024-03-16 06:02:34 +0000

    [Future][Backend] Implement NotePadController Feature Test

--- notepad/app/Models/NoteTag.php
+++ notepad/app/Models/NoteTag.php
@@ -10,6 +10,11 @@ class NoteTag extends Model
 {
     protected $table = 'notes_tags';
     protected $dates = ['deleted_at'];
+    protected $fillable = [
+        'note_id',
+        'tag_id',
+    ];
+    
     use HasFactory;
     use SoftDeletes;
 }
---
+++ notepad/tests/Feature/app/Http/Controllers/NotePadControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Tests\TestCase;
+
+use App\Models\Note;
+use App\Models\Tag;
+
+class NotePadControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * This method test  notepad getDetail function.
+     */
+    public function testgetDetail(): void 
+    {
+        // create fake data.
+        $note = Note::factory()->create();
+        $tagList = Tag::factory()->count(5)->create();
+        $note->tags()->attach($tagList);
+
+        // sent request.
+        $resp = $this->get('/notepad/' . $note->id);
+        $resp->assertStatus(200);
+    }
+
+    /**
+     * This method test update function.
+     */
+    public function testUpdate(): void
+    {
+        // create fake data.
+        $note = Note::factory()->create();
+        $tagList = Tag::factory()->count(5)->create();
+        $note->tags()->attach($tagList);
+        $UpdatedData = [
+            'title' => 'update title',
+            'content' => 'update content',
+            'tagIds' => $tagList->pluck('id')->toArray()
+        ];
+
+        // sent request. 
+        $resp = $this->post('/notepad/' . $note->id, $UpdatedData);
+        $resp->assertStatus(200);
+
+        $this->assertDatabaseHas('notes', [
+            'id' => $note->id,
+            'title' => 'update title',
+            'content' => 'update content'
+        ]);
+
+        foreach ($tagList as $tag) {
+            $this->assertDatabaseHas('notes_tags', [
+                'note_id' => $note->id,
+                'tag_id' => $tag->id
+            ]);
+        }
+    }
+}
